b77d29f3171b080d8033552ad89f82006b4c608f
Natalia Panina <93536861+nataliafront@users.noreply.github.com>
2021-11-10 22:50:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let select = () => {
+	let selectCurrent = document.querySelectorAll('.select__header'),
+		selectItem = document.querySelectorAll('.select__item');
+	selectCurrent.forEach((item) => {
+		item.addEventListener('click', (e) => {
+			e.currentTarget.parentElement.classList.toggle('select--opened');
+		})
+	});
+	selectItem.forEach((item) => {
+		item.addEventListener('click', (e) => {
+			e.currentTarget.closest('.select').querySelector('.select__current').innerHTML = e.currentTarget.innerHTML;
+			e.currentTarget.closest('.select').classList.remove('select--opened');
+		})
+	});
+};
+
+select();
